a10e79f1dfc82ca3aa56418c1e6ec267bd4fa168
Aaron Peckham <apeckham@gmail.com>
2019-09-16 21:01:27 -0700

    more interesting example

--- src/clj_cli_poc/picocli.clj
+++ src/clj_cli_poc/picocli.clj
@@ -3,12 +3,12 @@
 (import 'picocli.CommandLine$Option
         'picocli.CommandLine)
 
-(defrecord Address [^{picocli.CommandLine$Option {:names ["--street" "-s"]}} street
-                    ^{picocli.CommandLine$Option {:names ["--city" "-c"]}} city
-                    ^{picocli.CommandLine$Option {:names ["--state" "-st"]}} state
-                    ^{picocli.CommandLine$Option {:names ["--zip" "-z"]}} zip])
+(defrecord Address [^{CommandLine$Option {:names ["--street" "-s"]}} street
+                    ^{CommandLine$Option {:names ["--city" "-c"]}} city
+                    ^{CommandLine$Option {:names ["--state" "-st"]}} state
+                    ^{CommandLine$Option {:names ["--zip" "-z"]}} ^Integer zip])
 
 (defn -main [& args]
-  (let [address (map->Address {:zip 90210})]
+  (let [address (map->Address {})]
     (. (CommandLine. address) (parseArgs (into-array args)))
     (prn (into {} address))))
